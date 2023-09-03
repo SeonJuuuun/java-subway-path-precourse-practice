850560d4b6d88322de5542855180f9d59da608ce
yuseonjun <seonjun0906@gmail.com>
2023-09-03 15:51:46 +0900

    feat: 거리 로직 추가

--- src/main/java/subway/domain/PathDistances.java
+++ src/main/java/subway/domain/PathDistances.java
@@ -1,12 +1,31 @@
 package subway.domain;
 
 import java.util.List;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
 
 public class PathDistances {
 
-    private final List<Integer> distance;
+    private final WeightedMultigraph<Station, DefaultWeightedEdge> pathDistance =
+        new WeightedMultigraph<>(DefaultWeightedEdge.class);
 
-    public PathDistances(List<Integer> distance) {
-        this.distance = distance;
+    public PathDistances(List<Station> stations, List<Integer> distances) {
+        addStation(stations);
+        addStationDistance(stations, distances);
+    }
+
+    private void addStation(List<Station> stations) {
+        for (Station station : stations) {
+            pathDistance.addVertex(station);
+        }
+    }
+
+    private void addStationDistance(List<Station> stations, List<Integer> distances) {
+        for (int i = 0; i < stations.size() - 1; i++) {
+            pathDistance.setEdgeWeight(
+                pathDistance.addEdge(stations.get(i), stations.get(i + 1)),
+                distances.get(i)
+            );
+        }
     }
 }
