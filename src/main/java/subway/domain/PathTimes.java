61c822bd313e219bc3187cbfed8fac9ea234ad02
yuseonjun <seonjun0906@gmail.com>
2023-09-03 15:51:53 +0900

    feat: 시간 로직 추가

--- src/main/java/subway/domain/PathTimes.java
+++ src/main/java/subway/domain/PathTimes.java
@@ -1,5 +1,36 @@
 package subway.domain;
 
+import java.util.List;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.WeightedMultigraph;
+
 public class PathTimes {
+
+    private final WeightedMultigraph<Station, DefaultWeightedEdge> pathTime =
+        new WeightedMultigraph<>(DefaultWeightedEdge.class);
+
+    public PathTimes(List<Station> stations, List<Integer> times) {
+        addStation(stations);
+        addStationDistance(stations, times);
+    }
+
+    private void addStation(List<Station> stations) {
+        for (Station station : stations) {
+            pathTime.addVertex(station);
+        }
+    }
+
+    private void addStationDistance(List<Station> stations, List<Integer> times) {
+        for (int i = 0; i < stations.size() - 1; i++) {
+            pathTime.setEdgeWeight(
+                pathTime.addEdge(stations.get(i), stations.get(i + 1)),
+                times.get(i)
+            );
+        }
+    }
+
+    public WeightedMultigraph<Station, DefaultWeightedEdge> getPathDistance() {
+        return pathTime;
+    }
 
 }
