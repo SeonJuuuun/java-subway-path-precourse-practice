5d12e31dae4aaeacca55cd544f7c2916f358a803
yuseonjun <seonjun0906@gmail.com>
2023-09-03 15:53:47 +0900

    feat: 경로를 담당하는 PathController 생성

---
+++ src/main/java/subway/controller/PathController.java
@@ -0,0 +1,14 @@
+package subway.controller;
+
+import java.util.Scanner;
+import subway.domain.Screen;
+import subway.view.OutputView;
+
+public class PathController {
+
+    public void run(Scanner scanner) {
+        OutputView.loadView(Screen.MAIN);
+
+    }
+
+}
