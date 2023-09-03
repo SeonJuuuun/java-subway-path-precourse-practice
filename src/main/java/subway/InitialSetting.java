80c6ddc028567cc72cc37ea58827e387f21db983
yuseonjun <seonjun0906@gmail.com>
2023-09-03 15:51:14 +0900

    refactor: 양재시민의숲 누락 변경

--- src/main/java/subway/InitialSetting.java
+++ src/main/java/subway/InitialSetting.java
@@ -16,12 +16,12 @@ public class InitialSetting {
     }
 
     private static final List<String> STATIONS = List.of(
-        "교대역", "강남역", "역삼역", "남부터미널역", "양재역", "매봉역");
+        "교대역", "강남역", "역삼역", "남부터미널역", "양재역", "양재시민의숲역", "매봉역");
     private static final List<String> LINES = List.of("2호선", "3호선", "신분당선");
     private static final List<List<String>> PATH = List.of(
         List.of("교대역", "강남역", "역삼역"),
         List.of("교대역", "남부터미널역", "양재역", "매봉역"),
-        List.of("강남역", "양재역", "양재시민숲역"));
+        List.of("강남역", "양재역", "양재시민의숲역"));
     private static final List<List<Integer>> PATH_DISTANCES = List.of(
         List.of(2, 2),
         List.of(3, 6, 1),
